fbf89585aaade40771f14d28896fe34f7d6cac44
Andrew Burt <andrew@ampletorque.com>
2022-01-24 07:58:29 -0800

    change in tf file

--- lambda.tf
+++ lambda.tf
@@ -5,9 +5,5 @@ resource "aws_lambda_function" "iphelper_function" {
   handler          = "main"
   source_code_hash = "${filebase64sha256("./main.go")}"
   runtime          = "go1.x"
-  environment {
-    variables = {
-      KEY = "9a264276d60787ae3754f755eee8bb415885ab08fc627f0fa9a8123a"
-    }
-  }
+
 }
